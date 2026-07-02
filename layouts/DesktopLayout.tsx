6f28ef6dfed9e4875cd2d26e100db43f74a754cd
denshoelacee <sgywkdn@gmail.com>
2026-07-02 20:26:31 +0800

    adjusted

--- layouts/DesktopLayout.tsx
+++ layouts/DesktopLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import Footer from "../components/common/Footer";
 
 export default function AppLayout() {
