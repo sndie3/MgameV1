8955adb223e6ae05aafb4285097d18b9a46410dd
denshoelacee <sgywkdn@gmail.com>
2026-07-03 02:25:57 +0800

    revised

--- components/auth/disclaimer/DisclaimerCard.tsx
+++ components/auth/disclaimer/DisclaimerCard.tsx
@@ -54,7 +54,7 @@ export default function DisclaimerCard() {
         onClick={() => navigate("/login")}
         className="mt-6 w-full rounded-md bg-red-800 py-3 text-base font-semibold uppercase transition hover:bg-red-700 sm:text-lg cursor-pointer"
       >
-        Agree
+        I Agree
       </button>
     </div>
   );
