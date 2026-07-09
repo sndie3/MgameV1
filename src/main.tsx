2c4191c82e6fcc8cd70e715e16f1cfcef7965b08
Sandie <sandiegoles8@gmail.com>
2026-07-09 21:58:07 +0800

    app icon

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from '../context/ThemeProvider';
+import { applyAppIcon, getSavedAppIcon } from '../utils/appIcon';
+
+applyAppIcon(getSavedAppIcon());
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
