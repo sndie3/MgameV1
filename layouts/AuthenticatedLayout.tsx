6ed763df2ed5ab5f1a9c32ae8a411f144ff9bb90
Sandie <sandiegoles8@gmail.com>
2026-07-10 12:25:40 +0800

    add file

--- layouts/AuthenticatedLayout.tsx
+++ layouts/AuthenticatedLayout.tsx
@@ -2,9 +2,7 @@ import { Outlet } from "react-router-dom";
 
 
 export default function AuthenticatedLayout() {
-  // This Layout is used for authenticated routes, such as the dashboard and other protected pages.
-
-  // not yet implemented: if user is logged in, redirect to dashboard. If not, redirect to login page.
+ 
   return (
      <section className="min-h-dvh flex w-full bg-[var(--background-color)]">
       <main className="flex flex-1 flex-col overflow-y-auto">
