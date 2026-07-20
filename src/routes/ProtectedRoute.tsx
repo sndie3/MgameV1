61e8d06abf8bbc315e5804381f792b681b2dee1e
denshoelacee <sgywkdn@gmail.com>
2026-07-20 11:03:18 +0800

    Update

--- src/routes/ProtectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-
-function ProtectedRoute() {
-    // This component will check if the user is authenticated.
-    // If the user is authenticated, render the child routes.
-    // If the user is not authenticated, redirect to the login page.
-}
-
-export default ProtectedRoute
